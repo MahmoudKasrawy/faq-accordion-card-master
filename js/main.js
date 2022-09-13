850e7f3c17ea789ea5295cfec119bae285122523
mahmoud medhat <108630397+MahmoudKasrawy@users.noreply.github.com>
2022-09-13 15:33:04 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let allquestion=document.querySelectorAll('.question');
+allquestion.forEach(function(question){
+    question.addEventListener("click", function(e){
+
+        let answer = e.currentTarget.nextElementSibling;
+        let question = e.currentTarget.children[0];
+        let arrowIcon = e.currentTarget.children[1];
+        if (answer.style.display === "none") {
+          answer.style.display = "block";
+          question.style.fontWeight = "700";
+          arrowIcon.style.transform = "rotate(180deg)";
+        } else {
+          answer.style.display = "none";
+          question.style.fontWeight = "400";
+          arrowIcon.style.transform = "rotate(0deg)";
+        }
+
+
+
+})
+})
